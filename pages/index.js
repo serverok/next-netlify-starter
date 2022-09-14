76b6feecdccfc8471eb9126dded1daeedfb981b4
Yujin Boby <admin@serverok.in>
2022-09-14 13:23:21 +0530

    updated pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Welcome to ServerOK WordPress Services</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
