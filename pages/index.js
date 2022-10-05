a8bb5846ee6b4f511ee583e6a37eb7c37cbe7d53
Yujin Boby <admin@serverok.in>
2022-10-05 14:17:25 +0530

    added service page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,7 +14,8 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <Link href="https://serverok.in">ServerOk.in</Link><br/>
+          <Link href="/services">Services</Link>
         </p>
       </main>
 
