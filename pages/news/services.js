09599b433e87cc3bd7b290117a506e0b8bcf459e
Yujin Boby <admin@serverok.in>
2024-01-28 01:43:57 +0530

    updated

---
+++ pages/news/services.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+    return (
+        <>
+            <h1>First Post</h1>
+            <blockquote class="twitter-tweet" data-width="550" data-dnt="true">
+            <p lang="en" dir="ltr">This is why I love teaching, watch this dead on video from my <a href="https://t.co/occ8wKmlgm">https://t.co/occ8wKmlgm</a> webinar yesterday where I warned what would happen to the <a rel="nofollow" href="https://twitter.com/search?q=%24DIA&amp;src=ctag&amp;ref_src=twsrc%5Etfw">$DIA</a> <a rel="nofollow" href="https://twitter.com/search?q=%24SPY&amp;src=ctag&amp;ref_src=twsrc%5Etfw">$SPY</a> <a rel="nofollow" href="https://twitter.com/search?q=%24QQQ&amp;src=ctag&amp;ref_src=twsrc%5Etfw">$QQQ</a> with bad <a rel="nofollow" href="https://twitter.com/hashtag/CPI?src=hash&amp;ref_src=twsrc%5Etfw">#CPI</a> data &amp; why I had no overnight longs. Retweet/favorite this if you promise to be prepared &amp; STUDY HAAAAAARD! <a href="https://t.co/1MVRiljJU5">pic.twitter.com/1MVRiljJU5</a></p>
+            <p>— Timothy Sykes (@timothysykes) <a rel="nofollow" href="https://twitter.com/timothysykes/status/1569690756964646912?ref_src=twsrc%5Etfw">September 13, 2022</a></p></blockquote>
+            <p><script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p>
+
+            <div>
+                <Link href="/">Back to Home</Link>
+            </div>
+
+        </>
+    )
+}
